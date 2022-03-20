60adaefe8af27d8564aa8b842e62e34cf4399ff4
c.soler <c.soler13@usp.ceu.es>
2022-03-20 12:34:10 +0100

    Añadida las páginas de registro de alumno y empresa

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def main():
     return render_template('login.html')
 
+@app.route('/resgistroUsuario')
+def registro_usuario():
+    return render_template('resgistroUsuario.html')
+
+@app.route('/resgistroEmpresa')
+def registro_empresa():
+    return render_template('resgistroEmpresa.html')
+
 
 if __name__ == '__main__':
     app.run()
