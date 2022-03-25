4e2659a5b101d91bfa1d9b361bbb271dbc6f5e77
c.soler <c.soler13@usp.ceu.es>
2022-03-25 17:18:18 +0100

    el alumno se guarda correctamente en la base de datos

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -1,14 +1,46 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_migrate import Migrate
 
+from model.model import db, get_user_id, Alumno, get_user, add_to_db, get_user_by_id
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty@localhost:5432/JOBS'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = '8d438b8cca764385ae8652fefd10487c7eec02a7c5a6fb471ad8ccff0412405d'
+
+login_manager = LoginManager()
+login_manager.login_view = 'main'
+
+login_manager.init_app(app)
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@login_manager.user_loader
+def load_user(id):
+    return get_user_by_id(id)
 
 @app.route('/')
 def main():
     return render_template('login.html')
 
-@app.route('/resgistroUsuario')
+@app.route('/resgistroUsuario', methods=["GET", "POST"])
 def registro_usuario():
+    if request.method == 'POST':
+        id_reg = get_user_id()
+        nombreUsuario_reg = request.form.get('username')
+        nombre_reg = request.form.get('nombre')
+        apellido_reg = request.form.get('apellido')
+        contrasena_reg = request.form.get('pass')
+        telefono_reg = request.form.get('tel')
+        email_reg = request.form.get('email')
+        user = Alumno(alumno_id=id_reg, username=nombreUsuario_reg, password=contrasena_reg, nombre=nombre_reg,
+                      apellido=apellido_reg, telefono=telefono_reg, email=email_reg)
+
+        add_to_db(user)
+        flash("Usuario creado correctamente. Por favor inicie sesión con su usuario")
     return render_template('resgistroUsuario.html')
 
 @app.route('/resgistroEmpresa')
---
+++ Proyecto/model/model.py
@@ -0,0 +1,51 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def get_user(username):
+    return db.session.query(Alumno).filter_by(nombreUsuario=username).first()
+
+
+def get_user_id():
+    return db.session.query(Alumno.alumno_id).order_by(Alumno.alumno_id.desc()).first().alumno_id + 1
+
+
+def get_user_by_id(alumno_id):
+    return db.session.query(Alumno).filter_by(id=alumno_id).first()
+
+
+def add_to_db(e):
+    db.session.add(e)
+    db.session.commit()
+
+class Alumno(db.Model):
+    __tablename__ = 'alumnos'
+    alumno_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    nombre = db.Column(db.String)
+    apellido = db.Column(db.String)
+    telefono = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
+
+    def __init__(self, alumno_id, username, password, nombre, apellido, telefono, email):
+        self.alumno_id = alumno_id
+        self.username = username
+        self.password = password
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.email = email
+
+    def is_active(self):
+        return True
+
+    def is_authenticated(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.alumno_id)
